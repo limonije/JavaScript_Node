a93987d59abaca73616731ccc5ab838a08f20f8e
limonije <mroozen@live.nl>
2021-06-29 15:15:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hallo Winc Academy Studenten") 
+// Dit is een grote som
+/* Je ziet bovenstaande comment niet in de terminal als je de code runt,
+omdat comments niet worden gezien als code, maar bedoelt zijn om duidelijk
+te maken wat er in de code staat */
+/* let multiplication =  1230941 * 1823794; 
+console.log(multiplication);
+let division = 637263 / 54;
+console.log(division);*/
+/* Door een blok comment om de code te zetten, zet je de code als het ware uit. Node zal
+alleen de tekst Hallo Winc Academy Studenten laten zien */
